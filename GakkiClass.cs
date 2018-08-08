f869c78c0544c4c8484b7cfba8c8f9a842ee2f16
Yui <314754801@qq.com>
2018-08-08 17:24:14 +0800

    add delay class

---
+++ GakkiClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class GakkiClass
+{
+	public GakkiClass()
+	{
+        
+    }
+
+    //延迟
+    public void Delay(int milliSecond)
+    {
+        int start = Environment.TickCount;
+        while (Math.Abs(Environment.TickCount - start) < milliSecond)
+        {
+            Application.DoEvents(); //在大运算量循环内，加Application.DoEvents可以防止界面停止响应
+        }
+    }
+}
